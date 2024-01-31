d5626fc99bf6924da77698b30b49af59bac164b9
frfusch21 <reviewfarmei20@gmail.com>
2024-01-31 17:39:43 +0700

    First Commit Java

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,17 @@
+package com.example;
+public class Main {
+    public static void main(String[] args) {
+        //Sama kaya print
+        System.out.printf("Hello and welcome!");
+        //Print dengan newline
+        System.out.println("Nama saya Farah");
+        System.out.print("Ini tanpa spasi");
+
+        boolean flag;
+        //Looping dari 1 sampai 5
+        for (int i = 1; i < 5; i++) {
+            System.out.println("i = " + i);
+        }
+
+    }
+}
